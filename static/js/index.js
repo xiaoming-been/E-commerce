0f49bdaa37ef12a7fc9bbc6065816132f3e9dcc5
xiaoming-been <1294340611@qq.com>
2024-05-10 18:00:31 +0800

    update contents

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,4 @@
+/* 头部选项卡 */
 function openTab(evt, tabName) {
     var i, tab_content, tab_links;
     tab_content = document.getElementsByClassName("tab_content");
@@ -5,6 +6,20 @@ function openTab(evt, tabName) {
          tab_content[i].style.display = "none";
     }
     tab_links = document.getElementsByClassName("tab_links");
+    for (i = 0; i < tab_links.length; i++) {
+         tab_links[i].classList.remove("active");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.classList.add("active");
+}
+/* 总览选项卡 */
+function open_overview_tab(evt, tabName) {
+    var i, tab_content, tab_links;
+    tab_content = document.getElementsByClassName("overview_tab_content");
+    for (i = 0; i < tab_content.length; i++) {
+         tab_content[i].style.display = "none";
+    }
+    tab_links = document.getElementsByClassName("overview_tab_links");
     for (i = 0; i < tab_links.length; i++) {
          tab_links[i].classList.remove("active");
     }
