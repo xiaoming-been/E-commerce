c784828c21b8852e611cbae81dc029f32ca0d405
xiaoming-been <1294340611@qq.com>
2024-05-11 17:47:40 +0800

    update contetns

--- static/js/index.js
+++ static/js/index.js
@@ -11,6 +11,12 @@ function openTab(evt, tabName) {
     }
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.classList.add("active");
+    // 检查当前选项卡是否为第一个选项卡
+    if (tabName === "overview") {
+        document.getElementById("overview").style.display = "flex";
+    } else {
+        document.getElementById("overview").style.display = "none";
+    }
 }
 /* 总览选项卡 */
 function open_overview_tab(evt, tabName) {
