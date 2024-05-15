af8e9b9bf6230f4a2bd90a5d2581bbe4138570c5
xiaoming-been <1294340611@qq.com>
2024-05-15 23:17:49 +0800

    更新数据总览布局

--- static/js/index.js
+++ static/js/index.js
@@ -1,16 +1,25 @@
-/* 头部选项卡 */
+// 头部选项卡
+// 初始化页面时加载第一个选项卡的数据
+document.addEventListener('DOMContentLoaded', function () {
+    var tabs = document.querySelectorAll('.tab_links');
+    if (tabs.length > 0) {
+        tabs[0].classList.add('active');
+    }
+});
+
 function openTab(evt, tabName) {
     var i, tab_content, tab_links;
     tab_content = document.getElementsByClassName("tab_content");
     for (i = 0; i < tab_content.length; i++) {
-         tab_content[i].style.display = "none";
+        tab_content[i].style.display = "none";
     }
     tab_links = document.getElementsByClassName("tab_links");
     for (i = 0; i < tab_links.length; i++) {
-         tab_links[i].classList.remove("active");
+        tab_links[i].classList.remove("active");
     }
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.classList.add("active");
+
     // 检查当前选项卡是否为第一个选项卡
     if (tabName === "overview") {
         document.getElementById("overview").style.display = "flex";
@@ -18,17 +27,31 @@ function openTab(evt, tabName) {
         document.getElementById("overview").style.display = "none";
     }
 }
+
 /* 总览选项卡 */
+// 初始化页面时加载第一个选项卡的数据
+document.addEventListener('DOMContentLoaded', function () {
+    var tabs = document.querySelectorAll('.overview_tab_links');
+    if (tabs.length > 0) {
+        tabs[0].classList.add('active');
+    }
+});
 function open_overview_tab(evt, tabName) {
     var i, tab_content, tab_links;
     tab_content = document.getElementsByClassName("overview_tab_content");
     for (i = 0; i < tab_content.length; i++) {
-         tab_content[i].style.display = "none";
+        tab_content[i].style.display = "none";
     }
     tab_links = document.getElementsByClassName("overview_tab_links");
     for (i = 0; i < tab_links.length; i++) {
-         tab_links[i].classList.remove("active");
+        tab_links[i].classList.remove("active");
     }
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.classList.add("active");
+    // 检查当前选项卡是否为第一个选项卡
+    if (tabName === "overview_tab") {
+        document.getElementById("overview_tab").style.display = "flex";
+    } else {
+        document.getElementById("overview_tab").style.display = "none";
+    }
 }
